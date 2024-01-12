33d41c17ac25f900038ab1d2bd6fb3e588c0812c
John Mcnelis <jmcnelis@mt-509291.jpl.nasa.gov>
2024-01-11 22:28:16 -0500

    one more small change and then this is ready to test

--- s3-upload-info/rotate_upload_access_keys.py
+++ s3-upload-info/rotate_upload_access_keys.py
@@ -19,15 +19,15 @@ def update_local_creds(profile_name: str,
     try:
         config.read(aws_credentials_file)
         if config.get(profile_name, 'aws_access_key_id')==aws_access_key_id:
-            sys.exit(print(f"[{get_timestamp()}]", 'NO CHANGES, Local credentials for ', f'"{profile_name}"', ' are current.\n'))
+            sys.exit(print(f"[{get_timestamp()}]", 'NO CHANGES, Local credentials for ', f'"{profile_name}"', ' are current.'))
         config.set(profile_name, 'aws_access_key_id', aws_access_key_id)
         config.set(profile_name, 'aws_secret_access_key', aws_secret_access_key)            
         with open(aws_credentials_file, 'w+') as f:
             config.write(f)
     except Exception as e:
-        sys.exit(print(f"[{get_timestamp()}]", f'ERROR! Local credentials for "{profile_name}" may be expired.', default_contact_message, '\n'))
+        sys.exit(print(f"[{get_timestamp()}]", f'ERROR! Local credentials for "{profile_name}" may be expired.', default_contact_message))
     else:
-        print(f"[{get_timestamp()}]", 'KEYS ROTATED, Local credentials for ', f'"{profile_name}"', ' were rotated successfully.\n')
+        print(f"[{get_timestamp()}]", 'KEYS ROTATED, Local credentials for ', f'"{profile_name}"', ' were rotated successfully.')
 
 
 def get_active_creds(profile_name: str, region_name: str="us-east-1"):
@@ -42,9 +42,9 @@ def get_active_creds(profile_name: str, region_name: str="us-east-1"):
         created, status = secret['CreateDate'], secret['Status']
         del secret['CreateDate'], secret['Status']
     except ProfileNotFound as e:
-        sys.exit(print(f"[{get_timestamp()}]", f'ERROR! Local credentials are not stored for "{profile_name}".', default_contact_message, '\n'))
+        sys.exit(print(f"[{get_timestamp()}]", f'ERROR! Local credentials are not stored for "{profile_name}".', default_contact_message))
     except ClientError as e:
-        sys.exit(print(f"[{get_timestamp()}]", f'ERROR! Local credentials for "{profile_name}" are expired or otherwise invalid.', default_contact_message, '\n'))
+        sys.exit(print(f"[{get_timestamp()}]", f'ERROR! Local credentials for "{profile_name}" are expired or otherwise invalid.', default_contact_message))
     else:
         return secret, created, status
 
